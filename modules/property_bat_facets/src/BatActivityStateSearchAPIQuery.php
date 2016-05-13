fab8d3a2ad43366aaab0fe12c28ae131f5301e0a
Salvo Scala <salvoscala@gmail.com>
2016-05-13 17:02:18 +0200

    Add facet for activities

---
+++ modules/property_bat_facets/src/BatActivityStateSearchAPIQuery.php
@@ -0,0 +1,27 @@
+<?php
+
+class BatActivityStateSearchAPIQuery implements FacetapiQueryTypeInterface {
+  /**
+   * Implements FacetapiQueryTypeInterface::getType().
+   */
+  static public function getType() {
+    return 'bat_activity_state';
+  }
+
+  /**
+   * Implements FacetapiQueryTypeInterface::execute().
+   */
+  public function execute($query) {
+  }
+
+  /**
+   * Implements FacetapiQueryTypeInterface::build().
+   *
+   * Unlike normal facets, we provide a static list of options.
+   */
+  public function build() {
+    $build = array();
+    return $build;
+  }
+
+}
